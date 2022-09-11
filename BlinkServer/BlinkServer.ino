d7de853a23aa98dfad34a0bfc3ff6591af3cde07
patryk0493 <patryk0493@gmail.com>
2022-09-11 11:07:17 +0200

    working PoC

--- BlinkServer/BlinkServer.ino
+++ BlinkServer/BlinkServer.ino
@@ -3,11 +3,34 @@
 #include "StaticFiles.h"
 #include "ConnectionConfig.h"
 
+#define LED_BUILTIN 22
+
+#define INITIAL true
+
+bool ledOff = INITIAL;
+
+void readLed(Request &req, Response &res) {
+  res.print(!ledOff);
+}
+
+void readLed2(Request &req, Response &res) {
+  res.print(false);
+}
+
+void updateLed(Request &req, Response &res) {
+  ledOff = (req.read() != '0');
+  digitalWrite(LED_BUILTIN, !ledOff);
+  return readLed(req, res);
+} 
+
 WiFiServer server(80);
 Application app;
 
 void setup() {
   Serial.begin(115200);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, INITIAL);
 
   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
@@ -15,6 +38,10 @@ void setup() {
     Serial.print(".");
   }
   Serial.println(WiFi.localIP());
+
+  app.get("/led", &readLed);
+  app.get("/led2", &readLed2);
+  app.put("/led", &updateLed);
 
   app.use(staticFiles());
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const PATH = '/led';
+
 function App() {
+  const [ledOff, setLedOff] = useState<'0' | '1'>('0');
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleStateChange = () => {
+    const value = ledOff === '0' ? '1' : '0';
+    setIsLoading(true);
+    fetch(PATH, { method: 'PUT', body: value })
+      .then(response => response.text())
+      .then((state) => setLedOff(state === '0' ? '1' : '0'))
+      .then(() => setIsLoading(false));
+  }
+
+  useEffect(() => {
+    fetch(PATH)
+      .then(response => response.text())
+      .then(state => setLedOff(state === '0' ? '1' : '0'))
+      .then(() => setIsLoading(false));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <input
+        type={'checkbox'}
+        disabled={isLoading}
+        checked={ledOff === '1'}
+        onChange={handleStateChange}>
+      </input>
+    </header>
   );
 }
 
